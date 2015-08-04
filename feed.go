afe60f6b59cd63b4e59075929cf41b9167b33fce
Jens-Uwe Mager <jum@anubis.han.de>
2015-08-04 11:47:24 -0800

    Decode spot error messages.

--- feed.go
+++ feed.go
@@ -7,6 +7,7 @@ package spot
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -14,8 +15,21 @@ const (
 	SpotAPI = "https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/%v/message.json?start=%v"
 )
 
-type SpotFeed struct {
+type Error struct {
+	Code        string
+	Text        string
+	Description string
+}
+
+func (e Error) Error() string {
+	return fmt.Sprintf("%v: %v", e.Code, e.Text)
+}
+
+type Feed struct {
 	Response struct {
+		Errors struct {
+			Error Error
+		}
 		FeedMessageResponse struct {
 			Count int
 			Feed  struct {
@@ -38,14 +52,14 @@ type SpotFeed struct {
 
 func RetrieveMessages(feedID string) (messages []Message, err error) {
 	for {
-		var d SpotFeed
+		var d Feed
 		var resp *http.Response
 		resp, err = http.Get(fmt.Sprintf(SpotAPI, feedID, len(messages)))
 		if err != nil {
 			return
 		}
 		defer resp.Body.Close()
-		err = json.NewDecoder(resp.Body).Decode(&d)
+		d, err = DecodeFeed(resp.Body)
 		if err != nil {
 			return
 		}
@@ -54,6 +68,18 @@ func RetrieveMessages(feedID string) (messages []Message, err error) {
 		if len(messages) >= d.Response.FeedMessageResponse.TotalCount {
 			break
 		}
+	}
+	return
+}
+
+func DecodeFeed(r io.Reader) (f Feed, err error) {
+	err = json.NewDecoder(r).Decode(&f)
+	if err != nil {
+		return
+	}
+	if len(f.Response.Errors.Error.Code) > 0 {
+		err = f.Response.Errors.Error
+		return
 	}
 	return
 }
